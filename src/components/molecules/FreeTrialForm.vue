b051c56677709dca4cb7deca50ee23b27463a433
kdumagalhaes <ce.magalhaespereira@gmail.com>
2021-04-11 12:52:14 -0300

    delete Button component to handle form

--- src/components/molecules/FreeTrialForm.vue
+++ src/components/molecules/FreeTrialForm.vue
@@ -1,11 +1,16 @@
 <template>
     <form class="free-trial-form" @submit.prevent="checkForm" novalidate>
         <input
-            class="free-trial-form__input--error"
+            class="free-trial-form__input"
             placeholder="First Name"
             type="text"
-            :model-value="name"
-            @update:model-value="name = $event"
+            name="firstName"
+            :model="firstName"
+            v-bind:class="[
+                nameIsEmpty
+                    ? 'free-trial-form__input--error'
+                    : 'free-trial-form__input',
+            ]"
             autofocus
         />
         <p v-if="nameIsEmpty" class="free-trial-form__error-message">
@@ -16,8 +21,8 @@
             class="free-trial-form__input"
             placeholder="Last Name"
             type="text"
-            :model-value="lastName"
-            @update:model-value="lastName = $event"
+            name="lastName"
+            :model="lastName"
         />
         <p v-if="lastNameIsEmpty" class="free-trial-form__error-message">
             {{ errors[1] }}
@@ -27,8 +32,13 @@
             class="free-trial-form__input"
             placeholder="Email Address"
             type="email"
-            :model-value="email"
-            @update:model-value="email = $event"
+            name="email"
+            :model="email"
+            v-bind:class="[
+                emailIsNotValid
+                    ? 'free-trial-form__input--error'
+                    : 'free-trial-form__input',
+            ]"
         />
         <p v-if="emailIsNotValid" class="free-trial-form__error-message">
             {{ errors[2] }}
@@ -38,18 +48,14 @@
             class="free-trial-form__input"
             placeholder="Password"
             type="password"
-            :model-value="password"
-            @update:model-value="password = $event"
+            name="password"
+            :model="password"
         />
         <p v-if="passwordIsEmpty" class="free-trial-form__error-message">
             {{ errors[3] }}
         </p>
 
-        <Button
-            disabled="!formIsValid"
-            text="Claim your free trial"
-            type="submit"
-        />
+        <button class="free-trial-form__button">Claim your free trial</button>
         <TermsAndServicesWarning
             text="By clicking the button, you are agreeing to our "
             textLink="Terms and Services"
@@ -60,11 +66,10 @@
 
 <script>
 // atoms
-import { Button, TermsAndServicesWarning } from '@/components/atoms'
+import { TermsAndServicesWarning } from '@/components/atoms'
 
 export default {
     components: {
-        Button,
         TermsAndServicesWarning,
     },
 
@@ -76,7 +81,7 @@ export default {
                 'Looks like this is not an email',
                 'Password cannot be empty',
             ],
-            name: null,
+            firstName: null,
             lastName: null,
             email: null,
             password: null,
@@ -88,23 +93,11 @@ export default {
         }
     },
 
-    // passar valor pro input original informando pra mudar o CSS
-    // apresentar as mensagens de erro quando bater com as regras
-
     methods: {
         checkForm() {
             if (!this.validEmail(this.email)) {
                 this.emailIsNotValid = true
             }
-            if (!this.nameIsValid) {
-                this.nameIsEmpty = true
-            }
-            // if (this.lastName === null) {
-            //     this.lastNameIsEmpty = true
-            // }
-            // if (this.password === null) {
-            //     this.passwordIsEmpty = true
-            // }
         },
         validEmail(email) {
             return this.regex.test(email)
@@ -156,6 +149,34 @@ export default {
 
     &:focus {
         border: 2px solid $primary-red;
+    }
+}
+
+.free-trial-form__button {
+    text-transform: uppercase;
+    width: 100%;
+    height: 50px;
+    border-radius: $border-radius;
+    border: none;
+    background-color: $primary-green;
+    box-shadow: 0px 4px 0px 0px rgb(43, 166, 113);
+    transition: box-shadow 0.1s ease 0s, transform 0.1s ease 0s;
+    letter-spacing: 1px;
+    font-size: 1.1rem;
+    font-weight: $semi-bold;
+    color: #fff;
+    text-align: center;
+    margin: 20px 0 15px 0;
+
+    &:hover {
+        box-shadow: 0px 0px 0px 0px rgb(43, 166, 113);
+        transform: translateY(4px);
+    }
+}
+
+@media only screen and (max-width: $mobile) {
+    .free-trial-form__button {
+        font-size: 0.8rem;
     }
 }
 </style>
